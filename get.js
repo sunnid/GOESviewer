632e88a6c7de4b5cc82e5438ed8e638ec3a6f3d4
sunnid <chrishicks138@gmail.com>
2013-10-10 20:04:12 -0600

    Update get.js

--- get.js
+++ get.js
@@ -1,27 +1,6 @@
-function dateFormat (date, fstr, utc) {
-  utc = utc ? 'getUTC' : 'get';
-  return fstr.replace (/%[YmdHMS]/g, function (m) {
-    switch (m) {
-    case '%Y': return date[utc + 'FullYear'] (); // no leading zeros required
-    case '%m': m = 1 + date[utc + 'Month'] (); break;
-    case '%d': m = date[utc + 'Date'] (); break;
-    case '%H': m = date[utc + 'Hours'] (); break;
-    case '%M': m = date[utc + 'Minutes'] (); break;
-    case '%S': m = date[utc + 'Seconds'] (); break;
-    default: return m.slice (1); // unknown code, remove %
-    }
-    // add leading zero if required
-    return ('0' + m).slice (-2);
-  });
-}
-
-/* dateFormat (new Date (), "%Y-%m-%d %H:%M:%S", true) returns 
-   "2012-05-18 05:37:21"  */
-
 var http = require('http');
 var fs = require('fs');
 
-<<<<<<< HEAD
 
 
     var date = new Date();
@@ -54,18 +33,3 @@ var request = http.get("http://www.ssd.noaa.gov/goes/east/cp/rgb.jpg", function(
 
 
 
-
-=======
-var download = function(url, dest, cb) {
-  var file = fs.createWriteStream(dest);
-  var request = http.get(url, function(response) {
-    response.pipe(file);
-    file.on('finish', function() {
-      file.close();
-      cb();
-    });
-  });
-}
-
-download('http://www.ssd.noaa.gov/goes/east/cp/rgb.jpg', 'rgb.jpg.%H:%M:%S', function(){console.log('done!');});
->>>>>>> 10c5825d3ed463333366aca4277c4cc822dc02ad
