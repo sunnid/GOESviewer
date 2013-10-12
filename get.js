b089b4a55305423dd8fbec0d118cc224aec4b1e6
sunnid <chrishicks138@gmail.com>
2013-10-12 14:05:28 -0600

    Update get.js

--- get.js
+++ get.js
@@ -1,21 +1,156 @@
 var http = require('http');
 var fs = require('fs');
 var  pages =[
+
+ // every 30 minutes
+
     {
         name: 'weus_wv',
-        frequency: 1800000,
+        frequency: 3600000,
         url:'http://www.ssd.noaa.gov/goes/west/weus/wv.jpg'
     },
+    
     {
         name:'nhem_wv',
-        frequency:1800000,
+        frequency:3600000,
         url:'http://www.ssd.noaa.gov/goes/comp/nhem/wv.jpg'
     },
+    
     {
         name:'nhem_rgb',
-        frequency:1800000,
+        frequency:3600000,
         url:'http://www.ssd.noaa.gov/goes/comp/nhem/rgb.jpg'
-    }
+    },
+    
+    {
+        name:'bou_rgb',
+        frequency:3600000,
+        url:'http://www.ssd.noaa.gov/goes/east/wfo/bou/rgb.jpg'
+    },
+    
+    {
+        name:'bou_rb',
+        frequency:3600000,
+        url:'http://www.ssd.noaa.gov/goes/east/wfo/bou/rb.jpg'
+    },
+    
+    {
+        name:'bou_ir2',
+        frequency:3600000,
+        url:'http://www.ssd.noaa.gov/goes/east/wfo/bou/ir2.jpg'
+    },
+    
+    {
+        name:'central-plains_wv',
+        frequency:3600000,
+        url:'http://www.ssd.noaa.gov/goes/east/cp/wv.jpg'
+    },
+
+    {
+        name:'central-plains_rgb',
+        frequency:3600000,
+        url:'http://www.ssd.noaa.gov/goes/east/cp/rgb.jpg'
+    },
+    
+    {
+        name:'central-plains_rb',
+        frequency:3600000,
+        url:'http://www.ssd.noaa.gov/goes/east/cp/rb.jpg'
+    },
+    
+    {
+        name:'central-plains_ir2',
+        frequency:3600000,
+        url:'http://www.ssd.noaa.gov/goes/east/cp/ir2.jpg'
+    },
+    
+// every 270 seconds (4.5 minutes)
+        
+    {
+        name:'FTG_NTP',
+        frequency:270000,
+        url:'http://radar.weather.gov/RadarImg/NTP/FTG_NTP_0.gif'
+    },
+        
+    {   
+        name:'FTG_NCR',
+        frequency:270000,
+        url:'http://radar.weather.gov/RadarImg/NCR/FTG_NCR_0.gif'
+    },
+        
+    {
+        name:'FTG_N0Z',
+        frequency:270000,
+        url:'http://radar.weather.gov/RadarImg/N0Z/FTG_N0Z_0.gif'
+    },
+        
+    {
+        name:'FTG_N0V',
+        frequency:270000,
+        url:'http://radar.weather.gov/RadarImg/N0V/FTG_N0V_0.gif'
+    },
+        
+    {
+        name:'FTG_N0S',
+        frequency:270000,
+        url:'http://radar.weather.gov/RadarImg/N0S/FTG_N0S_0.gif'
+    },
+        
+    {
+        name:'FTG_N0R',
+        frequency:270000,
+        url:'http://radar.weather.gov/RadarImg/N0R/FTG_N0R_0.gif'
+    },
+        
+// every 15 minutes 
+        
+    {
+        name:'xray',
+        frequency:1800000,
+        url:'http://www.swpc.noaa.gov/sxi/images/latest_sxi.png'
+    },
+        
+    {
+        name:'cme',
+        frequency:1800000,
+        url:'http://sohowww.nascom.nasa.gov/data/realtime/c3/1024/latest.jpg'
+    },
+        
+    {
+        name:'aurora',
+        frequency:1800000,
+        url:'http://www.swpc.noaa.gov/pmap/gif/pmapN.gif'
+    },
+        
+// every 3 hours (10800 seconds) 
+        
+    {
+        name:'GWVS',
+        frequency:10800000,
+        url:'http://www.goes.noaa.gov/FULLDISK/GWVS.JPG'
+    },
+        name:'GEVS',
+        frequency:10800000,
+        url:'http://www.goes.noaa.gov/FULLDISK/GEVS.JPG'
+    },
+    
+    {
+        name:'GEIR',
+        frequency:10800000,
+        url:'http://www.goes.noaa.gov/FULLDISK/GEIR.JPG'
+    },
+
+    {
+        name:'MTVS',
+        frequency:10800000,
+        url:'http://www.goes.noaa.gov/FULLDISK/MTVS.JPG'
+    },
+
+    {
+        name:'GIVS',
+        frequency:10800000,
+        url:'http://www.goes.noaa.gov/FULLDISK/GIVS.JPG'
+    },
 ];
 
 
